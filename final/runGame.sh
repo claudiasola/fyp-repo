80fcbf48699d1594bb14f22619fa52ce5dd21392
claudiasola <claudiasola@dyn1192-158.wlan.ic.ac.uk>
2016-06-10 11:40:10 +0100

    AWS run parallel

--- final/runGame.sh
+++ final/runGame.sh
@@ -6,14 +6,14 @@ lower=3
 upper=9
 mean=5
 var=1.25
-COMPETITION_NUM=1000
+COMPETITION_NUM=10000
 #START=2
 
-python setupGame.pyc $agent_num $seed $lower $upper $mean $var $COMPETITION_NUM
+python setupGame.py $agent_num $seed $lower $upper $mean $var $COMPETITION_NUM
 
 #for COMPETITION_NUMBER in 10 
 #for COMPETITION_NUMBER in 10 100 200 300 500 600 800 950 
-for COMPETITION_NUMBER in {1..1000}  
+for COMPETITION_NUMBER in {2..2500}  
 #for (( c=$START; c<=$COMPETITION_NUM; c=$(($c+50))))
 do
 #	python game.py $agent_num $seed $lower $upper $c
